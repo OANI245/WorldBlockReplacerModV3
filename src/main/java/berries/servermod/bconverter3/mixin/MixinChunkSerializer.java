82da10f19801aa40cee4b8ebaa6d65dee14ca4e7
OANI245 <zu2ht@hotmail.com>
2026-01-31 13:07:57 +0800

    1

--- src/main/java/berries/servermod/bconverter3/mixin/MixinChunkSerializer.java
+++ src/main/java/berries/servermod/bconverter3/mixin/MixinChunkSerializer.java
@@ -4,22 +4,25 @@ import berries.servermod.bconverter3.Config;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Tuple;
+import net.minecraft.world.entity.ai.village.poi.PoiManager;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.ProtoChunk;
 import net.minecraft.world.level.chunk.storage.ChunkSerializer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 @Mixin(ChunkSerializer.class)
 public class MixinChunkSerializer {
-    @Redirect(method = "read", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/ListTag;getCompound(I)Lnet/minecraft/nbt/CompoundTag;", ordinal = 0))
-    private static CompoundTag read(ListTag instance, int i) {
-        CompoundTag compoundTag2 = instance.getCompound(i);
+    @Inject(method = "read", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/ListTag;getCompound(I)Lnet/minecraft/nbt/CompoundTag;", ordinal = 0, shift = At.Shift.AFTER))
+    private static void read(ServerLevel serverLevel, PoiManager poiManager, ChunkPos chunkPos, CompoundTag compoundTag, CallbackInfoReturnable<ProtoChunk> cir, @Local CompoundTag compoundTag2) {
         CompoundTag blockStatesBlock = compoundTag2.getCompound("block_states");
         ListTag list = blockStatesBlock.getList("palette", 10);
         for (int j = 0; j < list.size(); j++) {
@@ -80,7 +83,6 @@ public class MixinChunkSerializer {
         }
         blockStatesBlock.put("palette", list);
         compoundTag2.put("block_states", blockStatesBlock);
-        return compoundTag2;
     }
 
     @Unique
