a9e8369b5756347a527e24ccff5cb2fa7cee6ca5
OANI245 <zu2ht@hotmail.com>
2026-01-31 13:16:21 +0800

    License

--- src/main/java/berries/servermod/bconverter3/BlockReplacer.java
+++ src/main/java/berries/servermod/bconverter3/BlockReplacer.java
@@ -1,10 +1,9 @@
 package berries.servermod.bconverter3;
 
 import berries.servermod.bconverter3.commands.Commands;
-import berries.servermod.bconverter3.commands.ModCommand;
 import net.fabricmc.api.ModInitializer;
 
-public class BlockConverter implements ModInitializer {
+public class BlockReplacer implements ModInitializer {
     @Override
     public void onInitialize() {
         Config.INSTANCE.read();
